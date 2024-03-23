5ef21b92e03cd1b6732ee5242ebaf84bf10ab88e
CERMIX <143993991+CERMIX0317@users.noreply.github.com>
2024-03-23 19:47:10 +0900

    added

--- src/components/FlowerList.js
+++ src/components/FlowerList.js
@@ -9,37 +9,36 @@ import {
     Image,
     Button,
 } from 'semantic-ui-react'
+
 const FlowerList = ({flowers, onComplete, onRemove}) => {
+
+
     return (
         <div className="FlowerList">
-            <List divided verticalAlign='middle'>
-
+            <ol>
                 {flowers.map((flower, index) => {
                     return(
-                        <ListItem>
-                            <ListContent>
-                                <ListHeader as='b' className="Header">{flower.message}</ListHeader>
-                                <ListDescription>Sent by <b>  {flower.sender} </b> </ListDescription>
-                            </ListContent>
-                            <ListContent floated="right">
-                                <Button type="button" onClick={() => {
-                                    if (typeof onComplete === "function") {
-                                        onComplete(index);
-                                    }
-                                }}>수령
-                                </Button>
-                                <Button className="remove" type="button" onClick={() => {
-                                    if (typeof onRemove === "function") {
-                                        onRemove(index);
-                                    }
-                                }}>제거
-                                </Button>
-                                <br/>
-                            </ListContent>
-                        </ListItem>
+                        <li>
+                            <span className="Header">{flower.message}</span>
+                            <br/>
+                            <span>Sent by <b>  {flower.sender} </b> </span>
+                            <Button type="button" onClick={() => {
+                                if (typeof onComplete === "function") {
+                                    onComplete(index);
+                                }
+                            }}>수령
+                            </Button>
+                            <Button className="remove" type="button" onClick={() => {
+                                if (typeof onRemove === "function") {
+                                    onRemove(index);
+                                }
+                            }}>제거
+                            </Button>
+                            <br/>
+                        </li>
                     )
                 })}
-            </List>
+            </ol>
         </div>
     );
 }
