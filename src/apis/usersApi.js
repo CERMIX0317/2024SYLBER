210eac94e5e78bc1ccf6372b630b334ba016a307
hisohn1006snu <hisohn1006@snu.ac.kr>
2024-03-23 23:25:17 +0900

    Update usersApi.js

--- src/apis/usersApi.js
+++ src/apis/usersApi.js
@@ -4,10 +4,16 @@ const baseUrl = "3.92.72.2:3000"
 
 export const getUserApi = async (username) => {
     try {
-      const { data } = axios.get(`http://${baseUrl}/user/sjb`);
+      const { data } = axios.get(`http://${baseUrl}/user/sjb`).catch((error) => {
+        console.log('1');  
+        console.log(error);
+        
+      });
       console.log(data);
       return data;
     } catch (error) {
+      console.log(error.response.data);
+      console.log('1');
       throw error;
     }
   };
