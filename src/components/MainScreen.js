3dd3caee5f1bec02a5cea2575331635c5e61ac64
hisohn1006snu <hisohn1006@snu.ac.kr>
2024-03-24 03:58:18 +0900

    Update MainScreen.js

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -10,7 +10,7 @@ const seedPos = {seedX: window.innerWidth/2, seedY: window.innerHeight/2};
 function MainScreen({user}) {
   return (
     <div className="App">
-        <HeaderExampleUsersIcon/>
+        <HeaderExampleUsersIcon username = {user}/>
         <MenuExampleText/>
       <ButtonField seedPos = {seedPos} username = {user}/>
     </div>
