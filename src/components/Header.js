1ad448405d283224cf962375abad50b8df631a0d
CERMIX <143993991+CERMIX0317@users.noreply.github.com>
2024-03-24 03:46:59 +0900

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { HeaderContent, Header, Icon, Image } from 'semantic-ui-react'
 
-const HeaderExampleUsersIcon = () => (
-    <div style={{
-        borderRadius: 10,
-        backgroundColor: '#e5e5e5',
-        font: '20px Century Gothic',
-        padding: 20,
-    }}>
-        <Header as='h2' icon textAlign='center'>
-            <Icon name='heart' circular />
-            <HeaderContent>한송이</HeaderContent>
-        </Header>
+const HeaderExampleUsersIcon = ({username}) => (
+    <div>
+        <div style={{
+            borderRadius: 10,
+            backgroundColor: '#e5e5e5',
+            font: '18px Century Gothic',
+            padding: 10,
+        }}>
+            <span>{username}님 환영합니다! </span>
+            <Icon name='smile outline' circular verticalAlign='top'/> {/* verticalAlign='top' 추가 */}
+            <h1  style={{textAlign: 'center',
+                font: '48px Century Gothic',
+                padding: 20,}}>
+                <span>Hello Garden!</span>
+            </h1>
+        </div>
     </div>
 )
 
