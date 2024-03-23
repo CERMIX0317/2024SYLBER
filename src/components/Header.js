c9370d4412624c49e8a331929e6c3d4d6d7981b4
carsella0909 <163733902+carsella0909@users.noreply.github.com>
2024-03-24 04:02:24 +0900

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,17 @@
-import React from 'react'
-import { HeaderContent, Header, Icon, Image } from 'semantic-ui-react'
+import React from 'react';
+import { Header, Icon } from 'semantic-ui-react';
+import './HeaderExampleUsersIcon.css'; // Import CSS for styling
 
-const HeaderExampleUsersIcon = ({username}) => (
-    <div>
-        <div style={{
-            borderRadius: 10,
-            backgroundColor: '#e5e5e5',
-            font: '18px Century Gothic',
-            padding: 10,
-        }}>
-            <span>{username}님 환영합니다! </span>
-            <Icon name='smile outline' circular verticalAlign='top'/> {/* verticalAlign='top' 추가 */}
-            <h1  style={{textAlign: 'center',
-                font: '48px Century Gothic',
-                padding: 20,}}>
-                <span>Hello Garden!</span>
-            </h1>
-        </div>
+const HeaderExampleUsersIcon = ({ username }) => (
+  <div className="custom-header-container">
+    <div className="welcome-message">
+      <span>{username}님 환영합니다! </span>
+      <Icon name='smile outline' circular verticalAlign='top' />
     </div>
-)
+    <Header as="h1" textAlign="center" className="header-text">
+      <span>Hello Garden!</span>
+    </Header>
+  </div>
+);
 
-export default HeaderExampleUsersIcon
+export default HeaderExampleUsersIcon;
